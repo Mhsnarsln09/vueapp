9a094c42939f3fc0121cee16a6c0108d4f0f651f
Mhsnarsln09 <mhsnarsln09@gmail.com>
2022-10-23 18:21:35 +0300

    card

--- src/components/HomeComponents.vue
+++ src/components/HomeComponents.vue
@@ -1,8 +1,18 @@
 <template>
-  <v-container>
-    <v-row>
-     <v-col>
-        <h1>Home Page</h1>
+  <v-container >
+    <v-row >
+     <v-col >
+             <v-card class="mx-auto" max-width="344" height="300px">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+            cover
+          ></v-img>
+
+          <v-card-title> Top western road trips </v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+        </v-card>
      </v-col>
     </v-row>
   </v-container>
