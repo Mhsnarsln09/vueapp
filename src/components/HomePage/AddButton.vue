f73f032d8bf63672bc26c8c54104dc18ae1093b3
Mhsnarsln09 <mhsnarsln09@gmail.com>
2022-10-26 14:44:57 +0300

    26.10.2022 button

--- src/components/HomePage/AddButton.vue
+++ src/components/HomePage/AddButton.vue
@@ -1,7 +1,24 @@
 <template>
-  <v-btn color="white" class="bg-secondary" @click.stop="onAdd(items.char_id)">
-    <span class="text-white">Add</span>
-  </v-btn>
+  <div v-if="count" class="d-flex align-center flex-column" size="x-small">
+    <v-btn-toggle variant="outlined" divided style="height: 30px">
+      <v-btn class="bg-error ma-0" size="x-small" @click.stop=""> - </v-btn>
+      <v-btn size="x-small" class="ma-0" @click.stop="">
+        {{ count }}
+      </v-btn>
+      <v-btn class="bg-success ma-0" size="x-small" @click.stop="onAdd">
+        +
+      </v-btn>
+    </v-btn-toggle>
+  </div>
+  <div v-else>
+    <v-btn
+      color="white"
+      class="bg-secondary"
+      @click.stop="onAdd(items.char_id)"
+    >
+      <span class="text-white">Add</span>
+    </v-btn>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,9 +26,14 @@ import { defineComponent } from "@vue/runtime-core";
 
 export default defineComponent({
   props: ["items"],
+  data() {
+    return { count: 0 };
+  },
   methods: {
     onAdd(i: number) {
       console.log("add", i);
+      this.count++;
+      console.log("count", this.count);
     },
   },
 });
