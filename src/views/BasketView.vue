6b745d4e059a80f19c01b022705c95db0c4e0ab9
Mhsnarsln09 <mhsnarsln09@gmail.com>
2022-10-26 17:24:14 +0300

    basket v1.0

--- src/views/BasketView.vue
+++ src/views/BasketView.vue
@@ -1,16 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is a basket page</h1>
-  </div>
+  <v-app>
+    <v-table theme="dark">
+      <thead>
+        <tr>
+          <th class="text-left" @click.prevent="onLog">Name</th>
+          <th class="text-left">Calories</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in getItems" :key="item.name">
+          <td>{{ item.name }}</td>
+          <td>{{ item.nickname }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { mapGetters } from "vuex";
+import store from "@/store";
 export default defineComponent({
   data() {
     return {
       //
     };
+  },
+  methods: {
+    onLog() {
+      console.log("getItems", this.getItems);
+      console.log("store.getters", store.state.items);
+    },
+  },
+  computed: {
+    ...mapGetters(["getItems"]),
   },
 });
 </script>
